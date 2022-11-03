51ce2d00fdd8d8ba1661da04ffa55ba7fb7a910c
Albert Garde <albertsgarde@gmail.com>
2022-11-03 13:46:53 +0100

    :sparkles: Add utility function

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,7 @@ fn hash(x: u64) -> u64 {
 pub fn cent_diff(freq1: f32, freq2: f32) -> f32 {
     1200.0 * (freq2 / freq1).log2()
 }
+
+pub fn frequency_to_note_number(frequency: f32) -> f32 {
+    69.0 + 12.0 * (frequency / 440.0).log2()
+}
