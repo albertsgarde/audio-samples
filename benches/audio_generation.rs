85f6c7195138bcd9f3de33593898443f68ad3b66
Albert Garde <albertsgarde@gmail.com>
2022-11-10 16:53:21 +0100

    :sparkles: Add benchmarks

--- benches/audio_generation.rs
+++ benches/audio_generation.rs
@@ -1,7 +1,8 @@
 use audio_samples::parameters::{
     effects::EffectDistribution, oscillators::OscillatorTypeDistribution, DataParameters,
 };
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
+use flexblock_synth::modules::{ObjectSafeModule, PulseOscillator};
 use rand::distributions::Uniform;
 
 fn bench_parameters(c: &mut Criterion, label: &str, parameters: &DataParameters) {
@@ -101,6 +102,90 @@ pub fn bench(c: &mut Criterion) {
     for (label, parameters) in parameters.iter() {
         bench_parameters(c, label, parameters);
     }
+}
+
+pub fn oscillators(c: &mut Criterion) {
+    let base_parameters = DataParameters::new(44100, (50., 2000.), [0], 256);
+
+    let parameters = [
+        (
+            "osc_sine",
+            base_parameters
+                .clone()
+                .with_oscillator(OscillatorTypeDistribution::Sine, (0.5, 0.7)),
+        ),
+        (
+            "osc_saw",
+            base_parameters
+                .clone()
+                .with_oscillator(OscillatorTypeDistribution::Saw, (0.5, 0.7)),
+        ),
+        (
+            "osc_triangle",
+            base_parameters
+                .clone()
+                .with_oscillator(OscillatorTypeDistribution::Triangle, (0.5, 0.7)),
+        ),
+        (
+            "osc_pulse",
+            base_parameters.clone().with_oscillator(
+                OscillatorTypeDistribution::Pulse(Uniform::new(0.1, 0.9)),
+                (0.5, 0.7),
+            ),
+        ),
+        (
+            "osc_noise",
+            base_parameters.with_oscillator(OscillatorTypeDistribution::Noise, (0.5, 0.7)),
+        ),
+    ];
+
+    for (label, parameters) in parameters.iter() {
+        bench_parameters(c, label, parameters);
+    }
+}
+
+pub fn module(c: &mut Criterion) {
+    let frequency = 440.;
+    let duty_cycle = 0.5;
+    let inverse_sample_rate = 1. / 44100.;
+
+    let mut buffer = vec![0.; 256];
+
+    c.bench_function("direct", |b| {
+        b.iter_batched(
+            || (),
+            |_| {
+                let mut cur_pos = 0.;
+                for sample in buffer.iter_mut() {
+                    cur_pos += frequency * inverse_sample_rate;
+                    cur_pos %= 1.;
+                    if cur_pos < duty_cycle {
+                        *sample = 1.;
+                    } else {
+                        *sample = -1.;
+                    }
+                }
+
+                black_box(&buffer);
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    let module = PulseOscillator::new(frequency, duty_cycle, 44100).module();
+
+    c.bench_function("module", |b| {
+        b.iter_batched(
+            || module.clone(),
+            |mut module| {
+                for (i, sample) in buffer.iter_mut().enumerate() {
+                    *sample = module.next(i as u64);
+                }
+                black_box(&buffer);
+            },
+            BatchSize::SmallInput,
+        )
+    });
 }
 
 criterion_group!(benches, bench);
